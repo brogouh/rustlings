42a07458dfbbe9079959dbf6ff9596b549e38052
brogouh <nouh-master@outlook.fr>
2025-10-06 10:19:46 +0200

    finish enum

--- exercises/08_enums/enums3.rs
+++ exercises/08_enums/enums3.rs
@@ -44,8 +44,13 @@ impl State {
     }
 
     fn process(&mut self, message: Message) {
-        // TODO: Create a match expression to process the different message
-        // variants using the methods defined above.
+        match message {
+            Message::ChangeColor(red,green,blue) => self.change_color(red, green, blue),
+            Message::Resize { width, height } => self.resize(width, height),
+            Message::Echo(s) => self.echo(s),
+            Message::Quit => self.quit(), 
+            Message::Move(point) => self.move_position(point),
+        }
     }
 }
 
